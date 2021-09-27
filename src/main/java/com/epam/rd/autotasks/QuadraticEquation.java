3f50a161bb7b2eca6a0025082d47fd071785690d
Damir Delmeev <216345i@mail.ru>
2021-09-27 15:53:41 +0300

    Quadratic Equation

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -2,17 +2,33 @@ package com.epam.rd.autotasks;
 
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
 public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        if (a == 0) {
+            System.out.println("Error on input ");
+        } else {
+            double discriminant = findDiscriminant(a, b, c);
+            if (discriminant > 0) {
+                double firstRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+                double secondRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+                System.out.println(firstRoot + ", " + secondRoot);
+            }
+            if (discriminant == 0) {
+                double firstRoot = -b / (2 * a);
+                System.out.println(firstRoot);
+            }
+            if (discriminant < 0) {
+                System.out.println("no roots");
+            }
+        }
+    }
 
-
-
+    private static double findDiscriminant(double a, double b, double c) {
+        return (b * b) - (4 * a * c);
     }
 
 }
